62eb2fdf927b6e5621fb38afe0c888c88147c00d
munyengm <munyengm@users.noreply.github.com>
2022-10-12 13:08:26 -0400

    update

--- myEnv.sh
+++ myEnv.sh
@@ -3,13 +3,14 @@
 export WORK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "$WORK_ROOT"
 export DISPLAY=localhost:0.0
-export JAVA_HOME=$WORK_ROOT/programs/java/latest/
-export NODE=$WORK_ROOT/programs/node/latest/
-export PYTHONPATH=$WORK_ROOT/programs/python/python38
-export M2_HOME=$WORK_ROOT/programs/maven/latest
+export JAVA_HOME=$WORK_ROOT/programs/java/current/
+export NODE=$WORK_ROOT/programs/nodejs/current/
+export PYTHONPATH=$WORK_ROOT/programs/python/current
+export M2_HOME=$WORK_ROOT/programs/maven/current
 export PROJECT_ROOT=$WORK_ROOT/code/synergisms/
+export CMAKE="/C/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/"
 
-export PATH=$M2_HOME/bin:$JAVA_HOME/bin:$NODE/bin:$PYTHONPATH:$PYTHONPATH/Scripts:$PYTHONPATH/Lib:$YARN_HOME/bin:$CODEQL_HOME:$PROTOC_HOME/bin:$PATH:$PROJECT_ROOT/bin
+export PATH=$M2_HOME/bin:$JAVA_HOME/bin:$NODE/bin:$PYTHONPATH:$PYTHONPATH/Scripts:$PYTHONPATH/Lib:$YARN_HOME/bin:$CODEQL_HOME:$PROTOC_HOME/bin:$PATH:$PROJECT_ROOT/bin:$CMAKE/bin
 export JAVA_OPTS="${JAVA_OPTS} --add-opens java.base/java.util=ALL-UNNAMED \
 --add-opens java.base/java.lang.reflect=ALL-UNNAMED \
 --add-opens java.base/java.text=ALL-UNNAMED \
@@ -25,10 +26,10 @@ alias pushmaster='git push origin HEAD:refs/for/master'
 alias pushwip='git push origin HEAD:refs/for/wip'
 
 function activate_pyworking() {
-    if test -d "$WORK_ROOT/programs/python/venvs/working/Scripts"; then
-      . "${WORK_ROOT}/programs/python/venvs/working/Scripts/activate"
+    if test -d "$WORK_ROOT/programs/python/venv/working/Scripts"; then
+      . "${WORK_ROOT}/programs/python/venv/working/Scripts/activate"
     else
-      . "${WORK_ROOT}/programs/python/venvs/working/bin/activate"
+      . "${WORK_ROOT}/programs/python/venv/working/bin/activate"
     fi
 }
 
